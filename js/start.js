651faefd1795ddbb4561b2f2c85038ec46a1fb57
Ramielson <ramielsonedgar@gmail.com>
2024-03-22 16:10:42 +0200

    fix cards

---
+++ js/start.js
@@ -0,0 +1,32 @@
+"usu strict";
+
+window.addEventListener("scroll", stepCounter);
+
+const circles = document.querySelectorAll('svg #circle')
+const firstStep = document.querySelector('.list-features.step .list-features__item:first-child .list-features__header')
+
+function hasReached(el) {
+    if (el === null)
+        return false;
+    const topPosition = el.getBoundingClientRect().top;
+
+    if (window.innerHeight >= topPosition + el.offsetHeight)
+        return true;
+    else return false;
+}
+
+function stepCounter() {
+    if (!hasReached(firstStep)) return;
+
+    circles.forEach((circle, i) => {
+        let target = +circle.dataset.target;
+       
+        let strokeValue = 160 - 160 * (target / 100);
+        circle.style.setProperty("--target", strokeValue);
+    });
+
+
+    circles.forEach(
+        (el) => (el.style.animation = "progress 2s var(--ease-out-slow) forwards")
+    );
+}
